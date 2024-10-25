750ac0eabdcfa0031ae5074866b1a79300621b27
Cob Staines <cob.staines@gmail.com>
2024-10-25 15:01:23 -0500

    fix typo in na.omit

--- R/db_access.r
+++ R/db_access.r
@@ -270,7 +270,7 @@ tbl_join = function(dbcon, link, tbl=NA, join="left", by="pkey", columns=NA) {
   # load table if not provided
   if (is.na(tbl)[[1]]) {
     tbl = tbl(dbcon, link$root$table) %>%
-      select(any_of(omit.na(unique(unlist(c(
+      select(any_of(na.omit(unique(unlist(c(
         link$root$pkey,
         link$root$nkey,
         link$root$fkey,
