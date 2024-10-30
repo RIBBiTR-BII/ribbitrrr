899daf76e0eac9729759fd1953787b03fbbaa031
Cob Staines <cob.staines@gmail.com>
2024-10-30 16:32:02 -0500

    add on_load for check_package_version

--- R/zzz.r
+++ R/zzz.r
@@ -1,30 +1,33 @@
 #' Check package version against GitHub repository
-#' 
+#'
 #' @importFrom utils packageVersion installed.packages
 #' @keywords internal
 check_package_version <- function() {
   # Get current installed version
   current_version <- packageVersion("ribbitrrr")
-  
+
   # Try to fetch latest version from GitHub
   tryCatch({
     # Read DESCRIPTION file from main branch
     desc_url <- "https://raw.githubusercontent.com/RIBBiTR-BII/ribbitrrr/main/DESCRIPTION"
     desc_content <- readLines(desc_url)
-    
+
     # Extract version line
     version_line <- grep("^Version:", desc_content, value = TRUE)
     latest_version <- gsub("^Version:\\s*", "", version_line)
-    
+
     # Compare versions
     if (package_version(latest_version) > current_version) {
-      message(
-        "\nA newer version of ribbitrrr is available on GitHub (", 
-        latest_version, " > ", current_version, ").\n",
-        "Consider updating using:\n",
-        "remotes::install_github('RIBBiTR-BII/ribbitrrr')\n"
+      message <- sprintf(
+        "\nA newer version of ribbitrrr is available on GitHub (%s > %s).\n%s\n%s\n",
+        latest_version, current_version,
+        "Consider updating using:",
+        "remotes::install_github('RIBBiTR-BII/ribbitrrr')"
       )
+    } else {
+      message <- NULL
     }
+    return(message)
   }, error = function(e) {
     # Silently fail if unable to check version
     return(NULL)
@@ -32,6 +35,18 @@ check_package_version <- function() {
 }
 
 #' @keywords internal
+.onLoad <- function(libname, pkgname) {
+  # Perform the version check during package loading
+  update_message <- check_package_version()
+  # Store the message for later use in .onAttach
+  options(ribbitrrr_update_message = update_message)
+}
+
+#' @keywords internal
 .onAttach <- function(libname, pkgname) {
-  check_package_version()
+  # Display the update message, if any
+  update_message <- getOption("ribbitrrr_update_message")
+  if (!is.null(update_message)) {
+    packageStartupMessage(update_message)
+  }
 }
