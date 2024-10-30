d86295448488942015a78393339d544a25480b57
Cob Staines <cob.staines@gmail.com>
2024-10-30 18:07:12 -0500

    implement check_package_version

--- R/zzz.r
+++ R/zzz.r
@@ -1,20 +1,26 @@
 #' Check package version against GitHub repository
 #'
 #' @importFrom utils packageVersion installed.packages
+#' @importFrom httr GET content stop_for_status
 #' @keywords internal
 check_package_version <- function() {
   # Get current installed version
   current_version <- packageVersion("ribbitrrr")
+  # message("Current version: ", current_version)
 
   # Try to fetch latest version from GitHub
   tryCatch({
     # Read DESCRIPTION file from main branch
     desc_url <- "https://raw.githubusercontent.com/RIBBiTR-BII/ribbitrrr/main/DESCRIPTION"
-    desc_content <- readLines(desc_url)
+    response <- httr::GET(desc_url)
+    httr::stop_for_status(response)
+    desc_content <- httr::content(response, "text", encoding = "UTF-8")
+    # message("Successfully fetched DESCRIPTION file")
 
     # Extract version line
-    version_line <- grep("^Version:", desc_content, value = TRUE)
+    version_line <- grep("^Version:", strsplit(desc_content, "\n")[[1]], value = TRUE)
     latest_version <- gsub("^Version:\\s*", "", version_line)
+    # message("Latest version: ", latest_version)
 
     # Compare versions
     if (package_version(latest_version) > current_version) {
@@ -22,20 +28,21 @@ check_package_version <- function() {
         "\nA newer version of ribbitrrr is available on GitHub (%s > %s).\n%s\n%s\n",
         latest_version, current_version,
         "Consider updating using:",
-        "remotes::install_github('RIBBiTR-BII/ribbitrrr')"
+        "remotes::install_github('RIBBiTR-BII/ribbitrrr')\n"
       )
     } else {
       message <- NULL
     }
     return(message)
   }, error = function(e) {
-    # Silently fail if unable to check version
+    # warning("Error checking version: ", conditionMessage(e))
     return(NULL)
   })
 }
 
 #' @keywords internal
 .onLoad <- function(libname, pkgname) {
+  # message(".onLoad function called")
   # Perform the version check during package loading
   update_message <- check_package_version()
   # Store the message for later use in .onAttach
@@ -44,6 +51,7 @@ check_package_version <- function() {
 
 #' @keywords internal
 .onAttach <- function(libname, pkgname) {
+  # message(".onAttach function called")
   # Display the update message, if any
   update_message <- getOption("ribbitrrr_update_message")
   if (!is.null(update_message)) {
